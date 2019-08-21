3393067818d2ae0048a57a29fcb59aecf05e9ee2
AndreyGolubkow <AndreyGolubkow@gmail.com>
2019-08-22 04:16:42 +0700

    Поменял basepath

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,12 @@ module.exports = {
   pluginOptions: {
     meta: {
       projectName: 'My project',
-      url: 'https://example.com',
+      url: 'https://hostel5.herokuapp.com/office/',
       description: 'Lorem ipsum dolor sid amet',
       twitterHandle: 'example',
       socialImage: 'images/social.jpg',
-      googleAnalytics: 'UA-12345678-1'
+      googleAnalytics: 'UA-12345678-1',
     }
-  }
-}
+  },
+  publicPath: '/office/'
+};
