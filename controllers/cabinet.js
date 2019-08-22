9d8d8dc5d3bf5a7557fb4d48f2c96cc6976dc49b
AndreyGolubkow <AndreyGolubkow@gmail.com>
2019-08-23 02:09:55 +0700

    change setroom db error

--- controllers/cabinet.js
+++ controllers/cabinet.js
@@ -1,11 +1,15 @@
 let app = new (require('express').Router)();
 let models = require('../models');
+const model = require("../models");
 
 app.get('/cabinet/',function(req,res,next)
 {
   if(!req.user) return res.redirect('/');
   if(!req.user.room) return res.redirect('/cabinet/setroom');
   if(req.user.room.length === 0) return res.redirect('/cabinet/setroom');
+
+
+
 
   models.Floor.populate('rooms').findOrCreate({floor: req.user.room[0]}, {
     floor: req.user.room[0],
@@ -55,19 +59,22 @@ app.post('/cabinet/setroom',function(req,res,next)
   if(req.user.room) return res.redirect('../cabinet');
   if(!req.body.room) return res.redirect('../cabinet/setroom');
 
+  models.Room.findOrCreate({room: req.body.room}, {room: req.body.room})
+    .then(r => {
+      models.User.findOneAndUpdate({vkId: req.user.vkId}, {room: r.doc})
+        .then((d) => {
 
-  models.User.findOneAndUpdate({vkId: req.user.vkId}, {room: req.body.room})
-    .then((d) => {
-
-      var user = d;
-      req.login(user, function(error) {
-        if (!error) {
-          console.log('succcessfully updated user');
-        }
-      });
-      res.redirect('../cabinet');
-      res.end(); // important to update session
+          var user = d;
+          req.login(user, function(error) {
+            if (!error) {
+              console.log('succcessfully updated user');
+            }
+          });
+          res.redirect('../cabinet');
+          res.end(); // important to update session
+        });
     });
+
 });
 
 
